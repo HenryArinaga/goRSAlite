048e151464341d96cd8d016c611342d227b6da82
Henry Arinaga <henry.arinaga@icloud.com>
2026-06-26 01:37:38 -0700

    feat: method updated with check,radio,combo options

--- cmd/gui/screens/methods.go
+++ cmd/gui/screens/methods.go
@@ -1,6 +1,8 @@
 package screens
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
@@ -9,7 +11,22 @@ import (
 
 func MethodsScreen() fyne.CanvasObject {
 
-	inside := container.New(layout.NewVBoxLayout(), widget.NewLabel("this is the methods screen"))
+	check := widget.NewCheck("Optional", func(value bool) {
+		log.Println("Check set to", value)
+	})
+	radio := widget.NewRadioGroup([]string{"Option 1", "Option 2"}, func(value string) {
+		log.Println("Radio set to", value)
+	})
+	combo := widget.NewSelect([]string{"Option 1", "Option 2"}, func(value string) {
+		log.Println("Select set to", value)
+	})
+
+	inside := container.New(
+		layout.NewVBoxLayout(),
+		check,
+		radio,
+		combo,
+	)
 
 	return inside
 }
