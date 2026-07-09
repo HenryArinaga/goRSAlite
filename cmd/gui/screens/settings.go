6c4a0d16f1dd88b737d178dcbd96a9530a128eaf
Henry Arinaga <128102839+HenryArin@users.noreply.github.com>
2026-07-08 22:51:20 -0700

    feat: created cards for methods and placed them in the settings area for testing, nonclickable yet

--- cmd/gui/screens/settings.go
+++ cmd/gui/screens/settings.go
@@ -1,7 +1,10 @@
 package screens
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
@@ -9,7 +12,21 @@ import (
 
 func SettingsScreen() fyne.CanvasObject {
 
-	inside := container.New(layout.NewVBoxLayout(), widget.NewLabel("this is the settings screen"))
+	mainRectangle := canvas.NewRectangle(color.Black)
+	mainRectangle.SetMinSize(fyne.NewSize(500, 50))
+	trialDivison := widget.NewLabel("Trial Divison")
+	sqrt := widget.NewLabel("Square Root")
+	fermat := widget.NewLabel("Fermat Method")
 
-	return inside
+	content := container.New(layout.NewStackLayout(), mainRectangle, trialDivison)
+	content2 := container.New(layout.NewStackLayout(), mainRectangle, sqrt)
+	content3 := container.New(layout.NewStackLayout(), mainRectangle, fermat)
+
+	centered1 := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), content, layout.NewSpacer())
+	centered2 := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), content2, layout.NewSpacer())
+	centered3 := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), content3, layout.NewSpacer())
+
+	cards := container.New(layout.NewVBoxLayout(), centered1, centered2, centered3)
+
+	return cards
 }
