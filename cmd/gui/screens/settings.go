9bd7d06e9e2446cab7841c161ebcf9183d5b40d9
Henry Arinaga <128102839+HenryArin@users.noreply.github.com>
2026-07-10 22:31:32 -0700

    feat: added check ( its disabled for now) for lightmode)

--- cmd/gui/screens/settings.go
+++ cmd/gui/screens/settings.go
@@ -12,13 +12,24 @@ import (
 
 func SettingsScreen() fyne.CanvasObject {
 
+	lightModeCheck := widget.NewCheck("Light mode (coming soon)",
+		func(turnOnLightMode bool) {
+
+		})
+
 	recAppearance1 := canvas.NewRectangle(color.Black)
 	recAppearance1.SetMinSize(fyne.NewSize(500, 50))
+	lightModeCheck.Disable()
 
 	Appearance := widget.NewLabel("Appearance")
 	Appearance.TextStyle = fyne.TextStyle{Bold: true}
 	appearanceHeading1 := container.New(layout.NewCenterLayout(), Appearance, layout.NewSpacer())
-	appearanceBox := container.New(layout.NewVBoxLayout(), appearanceHeading1, recAppearance1, layout.NewSpacer())
 
-	return appearanceBox
+	lightModeRow := container.New(layout.NewStackLayout(), recAppearance1, lightModeCheck)
+
+	appearanceBox := container.New(layout.NewVBoxLayout(), appearanceHeading1, lightModeRow, layout.NewSpacer())
+
+	centered1 := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), appearanceBox, layout.NewSpacer())
+
+	return centered1
 }
