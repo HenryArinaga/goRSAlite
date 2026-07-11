d6ef0d8d4290871e2907bff3f0dc47fcd013b4cc
Henry Arinaga <128102839+HenryArin@users.noreply.github.com>
2026-07-11 01:24:24 -0700

    feat: filled out settings screen

--- cmd/gui/screens/settings.go
+++ cmd/gui/screens/settings.go
@@ -17,19 +17,55 @@ func SettingsScreen() fyne.CanvasObject {
 
 		})
 
+	benchmarkModeCheck := widget.NewCheck("Benchmark mode",
+		func(turnOnLightMode bool) {
+
+		})
+
+	sieveModeCheck := widget.NewCheck("Sieve mode (coming soon)",
+		func(turnOnLightMode bool) {
+
+		})
+
+	simdModeCheck := widget.NewCheck("SIMD Acceleration mode (coming soon)",
+		func(turnOnLightMode bool) {
+
+		})
 	recAppearance1 := canvas.NewRectangle(color.Black)
 	recAppearance1.SetMinSize(fyne.NewSize(500, 50))
 	lightModeCheck.Disable()
 
+	recAppearance2 := canvas.NewRectangle(color.Black)
+	recAppearance2.SetMinSize(fyne.NewSize(500, 50))
+
+	recAppearance3 := canvas.NewRectangle(color.Black)
+	recAppearance3.SetMinSize(fyne.NewSize(500, 50))
+	sieveModeCheck.Disable()
+
+	recAppearance4 := canvas.NewRectangle(color.Black)
+	recAppearance4.SetMinSize(fyne.NewSize(500, 50))
+	simdModeCheck.Disable()
+
 	Appearance := widget.NewLabel("Appearance")
 	Appearance.TextStyle = fyne.TextStyle{Bold: true}
-	appearanceHeading1 := container.New(layout.NewCenterLayout(), Appearance, layout.NewSpacer())
+	appearanceHeading := container.New(layout.NewCenterLayout(), Appearance, layout.NewSpacer())
+	lightModeRow := container.New(layout.NewStackLayout(), recAppearance1, layout.NewSpacer(), lightModeCheck)
+	appearanceBox := container.New(layout.NewVBoxLayout(), appearanceHeading, lightModeRow, layout.NewSpacer())
 
-	lightModeRow := container.New(layout.NewStackLayout(), recAppearance1, lightModeCheck)
+	optimizationSettings := widget.NewLabel("Optimization Settings")
+	optimizationSettings.TextStyle = fyne.TextStyle{Bold: true}
+	optimizationHeading := container.New(layout.NewCenterLayout(), optimizationSettings, layout.NewSpacer())
+	optimizationRow := container.New(layout.NewStackLayout(), recAppearance2, benchmarkModeCheck)
+	optimizationBox := container.New(layout.NewVBoxLayout(), layout.NewSpacer(), optimizationHeading, optimizationRow, layout.NewSpacer())
 
-	appearanceBox := container.New(layout.NewVBoxLayout(), appearanceHeading1, lightModeRow, layout.NewSpacer())
+	sieveRow := container.New(layout.NewStackLayout(), recAppearance3, sieveModeCheck)
+	sieveBox := container.New(layout.NewVBoxLayout(), layout.NewSpacer(), sieveRow, layout.NewSpacer())
 
-	centered1 := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), appearanceBox, layout.NewSpacer())
+	simdRow := container.New(layout.NewStackLayout(), recAppearance4, simdModeCheck)
+	simdBox := container.New(layout.NewVBoxLayout(), layout.NewSpacer(), simdRow, layout.NewSpacer())
 
-	return centered1
+	centered1 := container.New(layout.NewVBoxLayout(), layout.NewSpacer(), appearanceBox, optimizationBox, sieveBox, simdBox, layout.NewSpacer())
+
+	limitSize := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), centered1, layout.NewSpacer())
+	return limitSize
 }
