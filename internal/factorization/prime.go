0a93ecf92cd14169a2b2ec04af2565046721d176
Henry Arinaga <128102839+HenryArin@users.noreply.github.com>
2026-07-15 01:35:58 -0700

    feat: updated sieve method to include multiple numbers

--- internal/factorization/prime.go
+++ internal/factorization/prime.go
@@ -18,11 +18,21 @@ func IsPrime(number_to_factor int) bool {
 
 func Sieve(number_to_factor int) []int {
 	var res []int
+	isPrime := make([]bool, number_to_factor+1)
 	for i := 2; i <= number_to_factor; i++ {
-		if IsPrime(i) {
+		isPrime[i] = true
+	}
+	for i := 2; i*i <= number_to_factor; i++ {
+		if isPrime[i] == true {
+			for multiple := i * i; multiple <= number_to_factor; multiple += i {
+				isPrime[multiple] = false
+			}
+		}
+	}
+	for i := 2; i <= number_to_factor; i++ {
+		if isPrime[i] == true {
 			res = append(res, i)
 		}
 	}
-
 	return res
 }
