79767174144a37b9228703034c0ec7e994986346
Henry Arinaga <128102839+HenryArin@users.noreply.github.com>
2026-07-16 00:06:51 -0700

    feat: created the rsa screen

---
+++ cmd/gui/screens/rsa.go
@@ -0,0 +1,17 @@
+package screens
+
+import (
+	"goRSAlite/internal/controller"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+func RSAScreen(appController *controller.Controller) fyne.CanvasObject {
+
+	inside := container.New(layout.NewVBoxLayout(), widget.NewLabel("this is the logs screen"))
+
+	return inside
+}
